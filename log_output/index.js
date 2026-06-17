013a21d6b21030310819ff14f424b6cb303a6e7f
Rihhard-git <veskus@proton.me>
2026-06-17 13:21:27 +0300

    ch2 1.7

--- log_output/index.js
+++ log_output/index.js
@@ -1,10 +1,25 @@
+let currentString = ''
+
 const generateRandomString = () => {
 
     const randomString = crypto.randomUUID()
-
-    console.log(new Date, ":",randomString)
-
+    currentString = new Date + " : " + randomString
+    console.log(currentString)
     setTimeout(generateRandomString, 5000)
 }
 
-generateRandomString()
+import express from 'express'
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send(currentString)
+})
+
+
+app.listen(PORT, () => {
+
+    console.log(`Server started in port ${PORT}`)
+    generateRandomString()
+})
+
