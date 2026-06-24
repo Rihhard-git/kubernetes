27c14992d22861b5b82ca5998febf5bc558ae48f
Rihhard-git <veskus@proton.me>
2026-06-24 12:52:53 +0300

    ch3 2.5

--- log_output/file-reader/index.js
+++ log_output/file-reader/index.js
@@ -6,11 +6,15 @@ import { fileURLToPath } from 'url'
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const MESSAGE = process.env.MESSAGE
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const logFilePath = path.join(__dirname, 'files', 'logs.txt')
 const countFilePath = path.join(__dirname, 'files', 'count.txt')
+const informationPath = path.join(__dirname, 'config', 'information.txt')
+
+console.log('env variable MESSAGE: ', MESSAGE)
 
 
 app.get('/', async (req, res) => {
@@ -21,6 +25,15 @@ app.get('/', async (req, res) => {
             return data
         })
         .catch((err) => console.log('Error occured reading random string data: ', err))
+
+    console.log('trying to find configmap file')
+
+    const configMapText = await fs.readFile(informationPath, 'utf8')
+        .then((data) => {
+            console.log('file found with data: ', data)
+            return data
+        })
+        .catch((err) => console.log('Error occured reading confimapfile: ', err))
 
     console.log('trying to find count via network')
 
@@ -37,6 +50,8 @@ app.get('/', async (req, res) => {
 
     res.send(`
         <div>
+            <p>file content: ${configMapText}</p>
+            <p>env variable: MESSAGE=${MESSAGE}</p>
             <p>${randomString}</p>
             <p>Ping / Pongs: ${response.data}</p>
         </div>
