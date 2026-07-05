160542927a5dfb8d2997950c414a97d4583bf96c
Rihhard-git <veskus@proton.me>
2026-07-05 22:48:13 +0300

    testing backup

--- the_project/backup/script.sh
+++ the_project/backup/script.sh
@@ -3,10 +3,10 @@ set -e
 
 if [ $URL ]
 then
-  echo $URL
 
-  pg_dump -v $URL > /usr/src/app/backup.sql
-
+  #pg_dump -v $URL > /usr/src/app/backup.sql
+  curl -X GET -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+    "https://storage.googleapis.com/storage/v1/b/raihhardv-backup-bucket/o"
   echo "Not sending the dump actually anywhere"
   # curl -F ‘data=@/usr/src/app/backup.sql’ https://somewhere
 fi
