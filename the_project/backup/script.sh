df4fe53da9e459550874f315e5a486215c5f1a79
Rihhard-git <veskus@proton.me>
2026-07-06 13:45:49 +0300

    script

--- the_project/backup/script.sh
+++ the_project/backup/script.sh
@@ -4,11 +4,11 @@ set -e
 if [ $URL ]
 then
 
-  gcloud container clusters get-credentials dwk-cluster --location=europe-north1-b
+  pg_dump -v $URL > /usr/src/app/backup.sql
+  curl -X POST --data-binary @/usr/src/app/backup.sql \
+    -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+    "Content-Type: application/sql" \
+    "https://storage.googleapis.com/upload/storage/v1/b/raihhardv-backup-bucket/o"
 
-  #pg_dump -v $URL > /usr/src/app/backup.sql
-  curl -X GET -H "Authorization: Bearer $(gcloud auth print-access-token)" \
-    "https://storage.googleapis.com/storage/v1/b/raihhardv-backup-bucket/o"
-  echo "Not sending the dump actually anywhere"
   # curl -F ‘data=@/usr/src/app/backup.sql’ https://somewhere
 fi
