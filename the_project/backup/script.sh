a7117418df75aa0d230fcdfdc44e3cdc7a80962e
Rihhard-git <veskus@proton.me>
2026-07-06 14:22:55 +0300

    script

--- the_project/backup/script.sh
+++ the_project/backup/script.sh
@@ -3,12 +3,13 @@ set -e
 
 if [ $URL ]
 then
+  
 
   pg_dump -v $URL > /usr/src/app/backup.sql
   curl -X POST --data-binary @/usr/src/app/backup.sql \
     -H "Authorization: Bearer $(gcloud auth print-access-token)" \
     "Content-Type: application/sql" \
-    "https://storage.googleapis.com/upload/storage/v1/b/raihhardv-backup-bucket/o"
+    "https://storage.googleapis.com/upload/storage/v1/b/raihhardv-backup-bucket/o?name=backup$(date -I)"
 
   # curl -F ‘data=@/usr/src/app/backup.sql’ https://somewhere
 fi
